a897ea89c4ab9c1370f19d7f16bc8d5ba8c14353
lhe-acn-dump <141010344+lhe-acn-dump@users.noreply.github.com>
2023-08-01 11:59:09 +0800

    Update streamlit_app.py
    Change: to display watermelon under header

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,4 +26,4 @@ streamlit.header("Fruityvice Fruit Advice!")
 # New API for Fruityvice
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
