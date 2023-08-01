a78106aa3b55114f199fd713a169117618c20603
lhe-acn-dump <141010344+lhe-acn-dump@users.noreply.github.com>
2023-08-01 15:10:18 +0800

    Update streamlit_app.py
    Made some edits

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,4 +44,4 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 # add fruits
-add_fruit = streamlit.text_input('What fruit would you like to add?')
+add_fruit = streamlit.text_input('What fruit would you like to add','Enter fruit name here')
